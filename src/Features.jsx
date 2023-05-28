46198b1ddee2bfc5d55ca0ec4e3d08b412842417
yatharthb <yatharthbindal@gmail.com>
2023-05-28 01:14:16 -0500

    Added material UI to some components

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,13 +1,34 @@
 // components/Features.jsx
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './Features.css';
 
+const Card = ({ children, ...props }) => (
+  <motion.div
+    initial={{ opacity: 0, scale: 0.9 }}
+    animate={{ opacity: 1, scale: 1 }}
+    exit={{ opacity: 0, scale: 0.9 }}
+    whileHover={{ scale: 1.05 }}
+    transition={{ duration: 0.3 }}
+    className="feature-card"
+    {...props}
+  >
+    {children}
+  </motion.div>
+);
+
+const Icon = ({ children }) => (
+  <motion.div whileHover={{ rotate: 5 }} className="feature-icon">
+    {children}
+  </motion.div>
+);
+
 const Feature = ({ title, icon, description }) => (
-  <div className="feature-card">
-    <div className="feature-icon">{icon}</div>
+  <Card>
+    <Icon>{icon}</Icon>
     <h3 className="feature-title">{title}</h3>
     <p className="feature-description">{description}</p>
-  </div>
+  </Card>
 );
 
 const Features = () => {
@@ -33,9 +54,11 @@ const Features = () => {
     <div className="features">
       <h2 className="features-title">Our Premium Features</h2>
       <div className="features-grid">
-        {featureData.map((feature, index) => (
-          <Feature key={index} {...feature} />
-        ))}
+        <AnimatePresence>
+          {featureData.map((feature, index) => (
+            <Feature key={index} {...feature} />
+          ))}
+        </AnimatePresence>
       </div>
     </div>
   );
--- src/HowItWorks.jsx
+++ src/HowItWorks.jsx
@@ -1,14 +1,55 @@
 // components/HowItWorks.jsx
 import React from 'react';
-import './HowItWorks.css';
+import { Box, Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import { motion } from 'framer-motion';
 
-const Step = ({ number, title, description }) => (
-  <div className="step">
-    <div className="step-number">{number}</div>
-    <h4 className="step-title">{title}</h4>
-    <p className="step-description">{description}</p>
-  </div>
-);
+const HowItWorksContainer = styled(Box)(({ theme }) => ({
+  paddingTop: theme.spacing(4),
+  paddingBottom: theme.spacing(4),
+}));
+
+const StepCard = styled(Card)(({ theme }) => ({
+  maxWidth: 345,
+  marginBottom: theme.spacing(4),
+}));
+
+const StepNumber = styled(Typography)({
+  fontSize: '3rem',
+  fontWeight: 'bold',
+  color: 'primary.main',
+});
+
+const StepTitle = styled(Typography)(({ theme }) => ({
+  marginTop: theme.spacing(1),
+  marginBottom: theme.spacing(0.5),
+}));
+
+const StepDescription = styled(Typography)({
+  fontSize: '1rem',
+});
+
+const Step = ({ number, title, description }) => {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <StepCard>
+          <CardContent>
+            <StepNumber component="span">{number}</StepNumber>
+            <StepTitle variant="h5" component="h4">
+              {title}
+            </StepTitle>
+            <StepDescription component="p">{description}</StepDescription>
+          </CardContent>
+        </StepCard>
+      </motion.div>
+    </Grid>
+  );
+};
 
 const HowItWorks = () => {
   const stepsData = [
@@ -30,14 +71,18 @@ const HowItWorks = () => {
   ];
 
   return (
-    <div className="how-it-works">
-      <h2 className="how-it-works-title">How It Works</h2>
-      <div className="steps">
-        {stepsData.map((step, index) => (
-          <Step key={index} {...step} />
-        ))}
-      </div>
-    </div>
+    <HowItWorksContainer component="section">
+      <Container>
+        <Typography variant="h3" component="h2" align="center">
+          How It Works
+        </Typography>
+        <Grid container justifyContent="center" spacing={4}>
+          {stepsData.map((step, index) => (
+            <Step key={index} {...step} />
+          ))}
+        </Grid>
+      </Container>
+    </HowItWorksContainer>
   );
 };
 
